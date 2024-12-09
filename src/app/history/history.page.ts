c92ad717fda9554122c78561454306e38ee981fc
John Titor <joaossiqueira2002@gmail.com>
2024-12-09 12:54:51 -0300

    Melhorias
    colocando botão de historico.

--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -5,10 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss'],
 })
-export class HistoryPage  {
+export class HistoryPage {
+  conversions: any[] = []; // Lista de conversões para exibir
 
   constructor() { }
 
-  
+  // Carregar as conversões salvas quando a página for inicializada
+  ngOnInit() {
+    this.loadHistory();
+  }
 
+  // Função para carregar o histórico do localStorage
+  loadHistory() {
+    const conversionHistory = JSON.parse(localStorage.getItem('conversionHistory') || '[]');
+    this.conversions = conversionHistory; // Atribui o histórico à variável
+  }
 }
